984260d0420fedbbd7347d8069c61dd7f9043858
wa  ° <oncwnuOFQIPUqFMr48EisEyZQvmM@git.weixin.qq.com>
2020-04-21 19:00:52 +0800

    自动提交

--- Example/Pods/Target Support Files/Pods-SwiftProjects_Tests/Pods-SwiftProjects_Tests-frameworks.sh
+++ Example/Pods/Target Support Files/Pods-SwiftProjects_Tests/Pods-SwiftProjects_Tests-frameworks.sh
@@ -197,19 +197,17 @@ if [ -r "${ARTIFACT_LIST_FILE}" ]; then
 fi
 
 if [[ "$CONFIGURATION" == "Debug" ]]; then
-  install_framework "${BUILT_PRODUCTS_DIR}/NFAFile/NFAFile.framework"
+  install_framework "${BUILT_PRODUCTS_DIR}/FBSnapshotTestCase/FBSnapshotTestCase.framework"
   install_framework "${BUILT_PRODUCTS_DIR}/NFATipsUI/NFATipsUI.framework"
   install_framework "${BUILT_PRODUCTS_DIR}/NFAToolkit/NFAToolkit.framework"
-  install_framework "${BUILT_PRODUCTS_DIR}/FBSnapshotTestCase/FBSnapshotTestCase.framework"
   install_framework "${BUILT_PRODUCTS_DIR}/Nimble/Nimble.framework"
   install_framework "${BUILT_PRODUCTS_DIR}/Nimble-Snapshots/Nimble_Snapshots.framework"
   install_framework "${BUILT_PRODUCTS_DIR}/Quick/Quick.framework"
 fi
 if [[ "$CONFIGURATION" == "Release" ]]; then
-  install_framework "${BUILT_PRODUCTS_DIR}/NFAFile/NFAFile.framework"
+  install_framework "${BUILT_PRODUCTS_DIR}/FBSnapshotTestCase/FBSnapshotTestCase.framework"
   install_framework "${BUILT_PRODUCTS_DIR}/NFATipsUI/NFATipsUI.framework"
   install_framework "${BUILT_PRODUCTS_DIR}/NFAToolkit/NFAToolkit.framework"
-  install_framework "${BUILT_PRODUCTS_DIR}/FBSnapshotTestCase/FBSnapshotTestCase.framework"
   install_framework "${BUILT_PRODUCTS_DIR}/Nimble/Nimble.framework"
   install_framework "${BUILT_PRODUCTS_DIR}/Nimble-Snapshots/Nimble_Snapshots.framework"
   install_framework "${BUILT_PRODUCTS_DIR}/Quick/Quick.framework"
